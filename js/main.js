935fa6dde89e9358a9223832b4b7e1948e76f058
Sam Hopkins <safhopkins@yahoo.com>
2015-07-28 23:46:16 -0500

    New website

---
+++ js/main.js
@@ -0,0 +1,34 @@
+ $(document).ready(function(){
+	var s = skrollr.init();
+
+$("#about").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#slide-2").offset().top
+    }, 1500);
+});
+
+$("#projects").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#slide-4").offset().top
+    }, 1500);
+});
+
+$("#resume").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#slide-6").offset().top
+    }, 1500);
+});
+
+$("#contact").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#slide-8").offset().top + 300
+    }, 1500);
+});
+
+
+$("#home").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#slide-1").offset().top + 500
+    }, 1500);
+});
+});
